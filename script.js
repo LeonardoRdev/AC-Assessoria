fbc8f927ff82f1b0c343529d0ea705f7a0a15015
Leonardo Rocha <leonardorochaconstantino@gmail.com>
2025-02-21 20:01:12 -0300

    Responsividade do HEADER

--- script.js
+++ script.js
@@ -2,3 +2,16 @@
 // Infinite scroll
 
 // Arrumar responsividade do HEADER
+// Animação ao iniciar a página
+
+const menuHamburguer = document.querySelector("#menu-hamburguer");
+const navBar = document.querySelector("nav");
+menuHamburguer.onclick = (() => {
+    if (navBar.classList.contains("mostrar-nav")) {
+        navBar.classList.remove("mostrar-nav");
+    }
+
+    else {
+        navBar.classList.add("mostrar-nav");
+    }
+});
