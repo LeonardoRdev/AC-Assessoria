92e396a03d91290cc0e572b50cde9248058a1b8f
Leonardo Rocha <leonardorochaconstantino@gmail.com>
2025-02-24 19:53:02 -0300

    Pequenos ajustes e diversos elementos

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-// Sugeriram eu desfocar o fundo do "home" conforme for descendo
+// Sugeriram eu desfocar o fundo do "HOME" conforme for descendo
 // Infinite scroll
-
-// Arrumar responsividade do HEADER
 // Animação ao iniciar a página
-
-const menuHamburguer = document.querySelector("#menu-hamburguer");
-const navBar = document.querySelector("nav");
-menuHamburguer.onclick = (() => {
-    if (navBar.classList.contains("mostrar-nav")) {
-        navBar.classList.remove("mostrar-nav");
-    }
-
-    else {
-        navBar.classList.add("mostrar-nav");
-    }
-});
