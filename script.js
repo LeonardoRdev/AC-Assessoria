add0535a95f7d2fa4a121ee35a2fcb9c4bdbd6d7
Leonardo Rocha <leonardorochaconstantino@gmail.com>
2025-02-26 20:33:02 -0300

    Animação adicionada em vários elementos

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 // Sugeriram eu desfocar o fundo do "HOME" conforme for descendo
 // Infinite scroll
 // Animação ao iniciar a página
+
+const observer = new IntersectionObserver((entradas) => {
+    entradas.forEach((entrada) => {
+
+        // Adiciona a classe com a animação de "mostrar" quando o elemento aparecer na tela
+        if (entrada.isIntersecting) {
+            entrada.target.classList.add("mostrar");
+        }
+
+        // else {
+        //     entrada.target.classList.remove("mostrar");
+        // }
+    });
+});
+
+const elementosEscondidos = document.querySelectorAll(".esconder");
+elementosEscondidos.forEach((elemento) => {
+    observer.observe(elemento);
+});
