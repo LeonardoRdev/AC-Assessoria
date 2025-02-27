bc0ba731fc85e0567ea1f7c9688874a553b7bd68
Leonardo Rocha <leonardorochaconstantino@gmail.com>
2025-02-27 18:21:24 -0300

    Animação de BLUR ao scrollar

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-// Sugeriram eu desfocar o fundo do "HOME" conforme for descendo
-// Infinite scroll
-// Animação ao iniciar a página
-
+// Animações quando os elementos aparecerem na tela
 const observer = new IntersectionObserver((entradas) => {
     entradas.forEach((entrada) => {
 
         // Adiciona a classe com a animação de "mostrar" quando o elemento aparecer na tela
         if (entrada.isIntersecting) {
-            entrada.target.classList.add("mostrar");
+            entrada.target.classList.add("mostrar"); // POR QUE TARGET?? POR QUE TARGET?? POR QUE TARGET?? POR QUE TARGET?? POR QUE TARGET?? POR QUE TARGET?? POR QUE TARGET?? POR QUE TARGET?? POR QUE TARGET?? POR QUE TARGET?? 
         }
 
         // else {
@@ -20,3 +17,17 @@ const elementosEscondidos = document.querySelectorAll(".esconder");
 elementosEscondidos.forEach((elemento) => {
     observer.observe(elemento);
 });
+
+
+
+// SCROLL
+const wrapper = document.querySelector("#wrapper")
+const imagemFundoHome = document.querySelector("#imagem-home");
+
+// Adiciona BLUR na imagem do HOME ao scrollar para baixo
+wrapper.onscroll = () => {
+    let distanciaScroll = wrapper.scrollTop;
+    let qtdBlur = Math.min(distanciaScroll / 180, 6);
+
+    imagemFundoHome.style.filter = `blur(${qtdBlur}px)`
+}
